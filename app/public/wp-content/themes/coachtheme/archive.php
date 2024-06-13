f8b689b7a5846168c7b67e75d63b0132ae7643bf
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-06-13 12:23:24 -0600

    Add template content logic and previous/next pagination to archive template

--- app/public/wp-content/themes/coachtheme/archive.php
+++ app/public/wp-content/themes/coachtheme/archive.php
@@ -4,10 +4,12 @@
 <section class="page-wrap">
 
   <div class="container">
-        <h1><?php the_title();?></h1>
+        
+This is the archive template 
+        <?php get_template_part('includes/section', 'archive');?>
 
-        <?php get_template_part('includes/section', 'content');?>
-
+        <?php previous_posts_link();?>
+        <?php next_posts_link();?>
         
   </div>    
 </section>
