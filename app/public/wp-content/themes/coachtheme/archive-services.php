2fbb4ad1ff11d3fcdd587bdce0fe56a43d13fd16
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-06-30 21:37:44 -0600

    Adjust second sidebar for services archive  page and added flex wrap to services archive page

--- app/public/wp-content/themes/coachtheme/archive-services.php
+++ app/public/wp-content/themes/coachtheme/archive-services.php
@@ -4,14 +4,13 @@
 <div class="container">
 <section class="row">
 
-  <div class="col-lg-3">
+  <div class="col-lg-3 second-sidebar">
     <?php if( is_active_sidebar('blog-sidebar') ):?>
       <?php dynamic_sidebar('blog-sidebar');?>
     <?php endif;?>
   </div>
 
   <div class="col-lg-9">
-    This is the archive-section template
     <?php get_template_part('includes/section', 'archive');?>
     <?php previous_posts_link();?>
     <?php next_posts_link();?>
