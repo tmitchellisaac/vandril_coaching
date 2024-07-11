09c3f781ccc3f610a5eb63975236870d9366f068
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-07-11 11:15:31 -0600

    Cleaned up main.css

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -21,7 +21,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'vandril_coaching_1' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
